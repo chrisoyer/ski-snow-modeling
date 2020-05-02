d8a1c3c79132efffae26e01b981ccff8ceffe97b
Chris Oyer <chrisoyer@users.noreply.github.com>
2020-05-02 16:54:10 -0600

    working. need to generalize to all years and for both base depth and snowfall

---
+++ src/ots_snow_scrapy_scraper.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.linkextractors import LinkExtractor
+from string import Template
+import pandas as pd
+
+process = CrawlerProcess({'AUTOTHROTTLE_ENABLED': True,  # or download delay
+                          'HTTPCACHE_ENABLED': True,  # remove for final scrape to get live data
+                          'ROBOTSTXT_OBEY': True,
+                          'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
+                          'FEED_URI': '../data/ots_snowfall_data.json',
+                          'FEED_FORMAT': 'json',
+                          'FEED_EXPORT_ENCODING': '"utf-8"'})
+
+class OpenSnowSnowfallSpider(scrapy.Spider):
+    name = 'opensnow'
+    
+    links_df = pd.read_feather('../data/ots_station_links.feather').iloc[0:1, :]  # remove for full run
+    # load values and remove nested listing
+    start_urls = [x[0] for x in links_df.filter(items=['snow_link']).values.tolist()]
+
+    def parse(self, response, links_df=links_df):
+        month_xp = ".//div[starts-with(@class, 'cal_chart_di')]"
+        month_name_xp = ".//strong[@class='dte_mon']/text()"
+        date_xp = ".//span[@class='dte_hd']"
+        data_xp = "../div/text()"
+        data = {}
+        for month in response.xpath(month_xp):
+            month_name = month.xpath(month_name_xp).get()
+            data[month_name] = {}
+            for day in month.xpath(date_xp):
+                date = day.xpath("./text()").get()
+                day_data = day.xpath(data_xp).get()
+                # only save non-zero data. the later data cleaning step will use pd features
+                # to do datetime cleaning/interpolation
+                if date == r"\xa0" or day_data == "" or date is None or day_data is None:
+                    continue
+                data[month_name][date] = day_data
+        yield {'station': links_df.loc[links_df.snow_link == response.request.url, 'resort_name_short'].values[0],
+               'url': response.request.url,
+               'data': data,
+               }
+
+process.crawl(OpenSnowSnowfallSpider)
+process.start()
