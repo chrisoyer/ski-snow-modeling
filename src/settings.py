455321b675a652a52e2439398b68446cd2f2bcd3
Chris Oyer <chrisoyer@users.noreply.github.com>
2020-05-02 11:45:04 -0600

    settings for scrapy-splash scraper

---
+++ src/settings.py
@@ -0,0 +1,17 @@
+#############
+# for splash
+#############
+
+
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPLASH_URL = 'https://ec2-54-89-139-30.compute-1.amazonaws.com:8050'
+
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
